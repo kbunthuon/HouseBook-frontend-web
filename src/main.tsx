d911cd678c9b8e76e5b2d3108a306526c7ce9c88
ammin <ammin@student.unimelb.edu.au>
2025-09-05 14:21:31 +1000

    removed the tracking of node_modules, and added supabase client to test

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
+  import supabase from '../src/config/supabaseClient.ts';
 
+  console.log(supabase);
   createRoot(document.getElementById("root")!).render(<App />);
   
